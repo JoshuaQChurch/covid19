02b3152ff66955e8b17589c7b9714c817045b3bd
Joshua Church <joshuaqchurch@gmail.com>
2020-04-27 20:00:22 -0500

    added update option when running dashboard

--- R/run_app.R
+++ R/run_app.R
@@ -1,5 +1,16 @@
+#' Launch the COVID-19 dashboard
+#'
+#' @param update Fetch the latest data.
+#'
 #' @export
-dashboard <- function() {
+dashboard <- function(update = TRUE) {
+
+  if (update) {
+    message("Fetching latest data...")
+    download_all_data()
+  }
+  message("Launching dashboard...")
+
   app_dir <- system.file("apps", "covid-19", package = "covid19")
   shiny::runApp(app_dir, display.mode = "normal")
 }
